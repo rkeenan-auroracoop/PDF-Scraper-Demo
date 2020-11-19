1d98caa9bfd6116a7125271e2eda02ebe45b36cd
Ross Keenan <rkeenan@auroracoop.com>
2020-11-19 16:26:36 -0600

    Completed version.

--- PDF-Scraper-demo.py
+++ PDF-Scraper-demo.py
@@ -5,3 +5,50 @@ import pandas as pd
 from collections import namedtuple
 
 Line = namedtuple('Line', 'company_id company_name doctype reference currency voucher inv_date due_date open_amt_tc open_amt_bc current months1 months2 months3')
+
+company_re = re.compile(r'(V\d+) (.*) Phone:')
+line_re = re.compile(r'\d{2}/\d{2}/\d{4} \d{2}/\d{2}/\d{4}')
+
+file = 'Sample Report Pythonic.pdf'
+
+lines= []
+total_check = 0
+
+with pdfplumber.open(file) as pdf:
+    pages = pdf.pages
+    for page in pdf.pages:
+        text = page.extract_text()
+        for line in text.split('\n'):
+            comp = company_re.search(line)
+            if comp:
+                vend_no, vend_name = comp.group(1), comp.group(2)
+            elif line.startswith('INVOICES'):
+                doctype = "INVOICE"
+            elif line.startswith('CREDITNOTES'):
+                doctype = 'CREDIT NOTE'
+            elif line_re.search(line):
+                items = line.split()
+                lines.append(Line(vend_no, vend_name, doctype, *items))
+            elif line.startswith('Supplier total'):
+                tot = float(line.split()[2].replace(',', ''))
+                total_check += tot
+
+df = pd.DataFrame(lines)
+print(df.head())
+
+#print(df)
+
+#This converts Dates from objects to datetime[64]
+df['inv_date'] = pd.to_datetime(df['inv_date'])
+df['due_date'] = pd.to_datetime(df['due_date'])
+
+#converts dollar amounts to floats
+for col in df.columns[-6:]:
+    df[col] = df[col].map(lambda x: float(str(x).replace(',', '')))
+
+print(df['open_amt_bc'].sum())
+
+print(total_check)
+
+#False removes the indexes from Dataframe
+df.to_csv('invoices.csv', index=False)
